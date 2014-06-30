3c2d206a45896ad218e67a9b8b7adc893804a692
Alex Conway <alexconway808@gmail.com>
2014-06-29 16:10:59 -1000

    Initial commit of solution to sum of even fibonacci numbers

--- challenge_2.js
+++ challenge_2.js
@@ -1,6 +1,16 @@
 exports.sumOfEvenFibonacciNumbers = function(n){
   var sum = 0;
-  // do your work here
-  
+  var max = 4000000;
+  var container = [];
+
+  for (var i = 0; i < max; i++) {
+    container[i] = n(container, i);
+    if(container[i] > max) {
+      break;
+    }
+    if(container[i] % 2 == 0) {
+      sum += container[i]
+    }
+  }  
   return sum;
 };
