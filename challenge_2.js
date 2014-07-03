449c72692c25390a2c1940b53f6ffdf834bee5ca
Alex Conway <alexconway808@gmail.com>
2014-07-02 18:51:18 -1000

    printed out the first ten numbers of the fibonacci sequence

--- challenge_2.js
+++ challenge_2.js
@@ -1,16 +1,35 @@
 exports.sumOfEvenFibonacciNumbers = function(n){
+  
   var sum = 0;
-  var max = 4000000;
-  var container = [];
+  var x = 1; 
+  var y = 1;
+  var fib;
 
-  for (var i = 0; i < max; i++) {
-    container[i] = n(container, i);
-    if(container[i] > max) {
-      break;
-    }
-    if(container[i] % 2 == 0) {
-      sum += container[i]
-    }
-  }  
-  return sum;
+ for (var i = 1; i < n; i++){
+  fib = x + y;
+  console.log('current fib: ' + fib);
+  x = y;
+  console.log('new x value: ' + x);
+  y = fib;
+  console.log('what is new y: ' + y);
+  sum += x;
+ }
+ // console.log (sum); 
+
 };
+
+
+  // var fib = x + y
+  // x = y
+  // y = fib
+  //console log
+
+
+//   for (var i = 0; test2 < n; i++) {
+//     if(i % 2 === 0) {
+//       sum += test2;
+//     }
+//     console.log(sum);
+//   }  
+//   return sum;
+// };
