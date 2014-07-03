a9496132dc5521b2b16d05fc5cd44c7570d7c9bd
Alex Conway <alexconway808@gmail.com>
2014-07-02 19:28:35 -1000

    both tests pass for 10 and 4 million

--- challenge_2.js
+++ challenge_2.js
@@ -7,32 +7,11 @@ exports.sumOfEvenFibonacciNumbers = function(n){
 
  for (var i = 1; fib < n; i++){
   fib = x + y;
-  // console.log('current fib: ' + fib);
   x = y;
-  // console.log('new x value: ' + x);
   y = fib;
-  // console.log('what is new y: ' + y);
   if(fib % 2 === 0){
-    // console.log(fib);
     sum += fib;  
   }
  }
   return sum;
-
 };
-
-//   for (var i = 0; test2 < n; i++) {
-//     if(i % 2 === 0) {
-//       sum += test2;
-//     }
-//     console.log(sum);
-//   }  
-//   return sum;
-// };
-
-
-  // var fib = x + y
-  // x = y
-  // y = fib
-  //console log
-
